a8a53d1f3772c1e35fb1f7dad40680cb563dd2a3
Damar Buana Murti <damarbuanamurti@gmail.com>
2024-12-18 13:12:32 +0700

    style: add curly braces

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -4,15 +4,16 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) {
-    return res.sendStatus(401).json({ error: "Unauthenticated" });
+  if (!token) {
+    return res.status(401).json({ error: "Unauthenticated" });
   }
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403).json({ error: "Unauthorized" });
+    if (err) {
+      return res.status(403).json({ error: "Unauthorized" });
+    }
 
     req.user = user;
-
     next();
   });
 };
