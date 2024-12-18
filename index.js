761d251eea3f40747347ceda9d4fed846fbae783
Damar Buana Murti <damarbuanamurti@gmail.com>
2024-12-18 13:12:06 +0700

    feat: add error middleware

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const usersRouter = require("./routers/users.router");
 const transactionsRouter = require("./routers/transactions.router");
+const globalErrorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 const port = 8080;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -14,10 +16,10 @@ app.use(
   })
 );
 
-app.use(cors());
-
 app.use(usersRouter);
 app.use(transactionsRouter);
+
+app.use(globalErrorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
